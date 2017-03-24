7819110d228fd249cdc89f3cf31f156fb5b2047e
Herald Gutierrez <heraldgutierrez@hotmail.com>
2017-03-24 12:09:49 -0500

    add privacy policy for us of IG api

--- routes/index.js
+++ routes/index.js
@@ -7,27 +7,7 @@ module.exports = (function() {
 	router.get('/', function(req, res) { res.render('index', { title: 'Home' }); });
 	router.get('/Wedding', function(req, res) { res.render('wedding', { title: 'Wedding' }); });
 	router.get('/Wedding-Social', function(req, res) { res.render('wedding-social', { title: 'Wedding Social' }); });
-
-	// router.get('/Portfolio', function(req, res) { res.render('portfolio'); });
-
-	// router.get('/Portfolio/Scoreboard', function(req, res) { res.redirect('/Portfolio'); });
-	// router.get('/Portfolio/Scoreboard/Simple', function(req, res) { res.render('portfolio/simple-scoreboard'); });
-	// router.get('/Portfolio/Scoreboard/LED', function(req, res) { res.render('portfolio/led-scoreboard'); });
-	// router.get('/Portfolio/Scoreboard/LEDSimple', function(req, res) { res.render('portfolio/led-simple'); });
-
-	// router.get('/Portfolio/Cards', function(req, res) { res.render('portfolio/cards'); });
-	
-	// router.get('/Portfolio/Casino/VideoPoker', function(req, res) { res.render('portfolio/casino'); });
-
-	// router.get('/Portfolio/GymLocker', function(req, res) { res.render('portfolio/gymlocker'); });
-
-	// router.get('/Portfolio/MIPS', function(req, res) { res.render('portfolio/mips'); });
-
-	// router.get('/Portfolio/NumberMunchers', function(req, res) { res.render('portfolio/numbermunchers'); });
-
-
-	// Christmas Wishlist
-	// router.get('/CousinsChristmas2016', function(req, res) { res.render('cousins-wishlist'); });
+	router.get('/Privacy-Policy', function(req, res) { res.render('privacy-policy', { title: 'Privacy Policy' }); });
 
 	return router;
 })();

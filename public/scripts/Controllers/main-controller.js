e7262abaede0b13ee117f6748bf2122dfff7ac07
Herald Gutierrez <heraldgutierrez@hotmail.com>
2017-03-29 19:33:50 -0500

    Layout - Updated mobile navigation

--- public/scripts/Controllers/main-controller.js
+++ public/scripts/Controllers/main-controller.js
@@ -2,6 +2,7 @@ app.controller('mainController', ['$scope', function($scope) {
 	var self = this;
 
 	self.showMobile = false;
+	
 	self.mobileMenuClick = function()
 	{
 		self.showMobile = !self.showMobile;
